6131a567bfd4721e5414320d8de8f16cf4c65483
vb <2234024474@qq.com>
2024-08-27 17:42:29 +0800

    [daily store]

--- main.c
+++ main.c
@@ -1,19 +1,51 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include"stdio.h"
+#include"stdlib.h"
+#include"string.h"
+typedef struct Node{
+int data;
+struct Node *next;
+struct Node*prev;
+}*zjlist;
+zjlist create_node(int data)
+{
+zjlist temp=(zjlist)malloc(sizeof(struct Node));
+if(temp==NULL)
+{
+    printf("malloc error\n");
+    exit(0);
+}
+temp->data=data;
+//temp->next=temp;
+//temp->prev=temp;
+temp->next=NULL;
+temp->prev=NULL;
+return temp;
+}
+void print_list(zjlist head)
+{
+    zjlist temp=(zjlist)malloc(sizeof(struct Node));
+    while(head->next!=NULL)
+    {
+        printf("%d\n",head->data);
+        head=head->next;
+    }
+}
+zjlist insert_node(zjlist head,int data)
+{
+    zjlist temp=create_node(data);
+    temp->next=head;
+    temp->prev=head->prev;
+    head->prev=temp;
+    head->prev->next=temp;
+    return head;
+}
+
 int main(void)
 {
-int n=0;
-while (~scanf("%d", &n))
-{
-    if(n%2==0)
-    {
-        printf("even\n");
-    }
-    else
-    {
-        printf("odd\n");
-    }   
-}
+    zjlist head=create_node(0);
+   head= insert_node(head,1);
+    head=insert_node(head,2);
+        print_list(head);
+  //  printf("head is %d\n",head->data);
 return 0;
 }
